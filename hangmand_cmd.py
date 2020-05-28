6d724c6bcd8bae4b955dfc91f10ec876c0f8effd
jp-nullpod <jp.fortuno@nullpod.com>
2020-05-28 17:49:27 +0400

    added display _ for the mystery word

--- hangmand_cmd.py
+++ hangmand_cmd.py
@@ -13,11 +13,28 @@ def pickRandomWord(words):
     index = random.randint(0,total)
     return words[index]
 
+def initGuessedWord(mysteryWord):
+    """a utility function to display the underscores with spaces for the hidden mystery word"""
+    guessedWord = ''
+    wordLength = len(mysteryWord)
+    
+    for i in range(wordLength):
+        guessedWord += '_'
+
+    return guessedWord
+
 def playGame():
     # builds a list of words from the hangman word file
     words = createPlayBoard()
     # pick a random word from the list created
     mysteryWord = pickRandomWord(words)
+    # default number of tries in a game, this might as well be in a config file
+    numberOfTries = 10
+    # display _ for the mystery word
+    guessedWord = initGuessedWord(mysteryWord)
+    win = False
+    # array to keep letters already used before by the user
+    alreadyUsed = []
 
 def main():
     answer = 'y'
