f77a663704874392cf804a95ab35bb81a405ce57
jp-nullpod <jp.fortuno@nullpod.com>
2020-05-28 17:41:32 +0400

    Create PlayBoard

--- hangmand_cmd.py
+++ hangmand_cmd.py
@@ -1,6 +1,15 @@
-def playGame():
-    pass
 
+def createPlayBoard():
+    words = []
+    file = open('./hangman_words_db','r')
+    for line in file:
+        words.append(line.rstrip())
+    file.close()
+    return words
+
+
+def playGame():
+    words = createPlayBoard()
 
 def main():
     answer = 'y'
