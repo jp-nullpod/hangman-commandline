9a5d093a550938c89293709d45a684a1b9ad310e
jp-nullpod <jp.fortuno@nullpod.com>
2020-05-28 17:45:11 +0400

    pickRandomWord function added

--- hangmand_cmd.py
+++ hangmand_cmd.py
@@ -3,13 +3,20 @@ def createPlayBoard():
     words = []
     file = open('./hangman_words_db','r')
     for line in file:
-        words.append(line.rstrip())
+        words.append(line.rstrip()) # remove unnecessary spaces from the list
     file.close()
     return words
 
+def pickRandomWord(words):
+    total = len(words)-1
+    index = random.randint(0,total)
+    return words[index]
 
 def playGame():
+    # builds a list of words from the hangman word file
     words = createPlayBoard()
+    # pick a random word from the list created
+    mysteryWord = pickRandomWord(words)
 
 def main():
     answer = 'y'
