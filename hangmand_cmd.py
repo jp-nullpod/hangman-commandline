93abdc13d582990012223c716bd32b3fe6ef5c8a
jp-nullpod <jp.fortuno@nullpod.com>
2020-05-28 17:50:50 +0400

    added hangman logic

--- hangmand_cmd.py
+++ hangmand_cmd.py
@@ -22,6 +22,26 @@ def initGuessedWord(mysteryWord):
         guessedWord += '_'
 
     return guessedWord
+def listToString(s):  
+    str1 = ''  
+    for ele in s:  
+        str1 += ele   
+ 
+    return str1  
+
+def hangmanLogic(mysteryWord, letterEntered, guessedWord):    
+    index = 0
+    while index < len(mysteryWord):
+        index = mysteryWord.find(letterEntered, index)
+        if index == -1:
+            break
+
+        temp = list(guessedWord)
+        temp[index] = letterEntered
+        guessedWord = listToString(temp)
+        index += 1
+
+    return guessedWord
 
 def playGame():
     # builds a list of words from the hangman word file
@@ -35,6 +55,28 @@ def playGame():
     win = False
     # array to keep letters already used before by the user
     alreadyUsed = []
+
+    while numberOfTries > 0:
+        letterEntered = ''
+        while len(letterEntered) != 1 or letterEntered in alreadyUsed:
+            letterEntered = input('Please enter a letter: ')
+            if letterEntered in alreadyUsed:
+                print('You have already typed this character before\n')
+                alreadyUsed.sort()
+                print('Already used ', alreadyUsed)
+            
+        alreadyUsed.append(letterEntered)
+        previousGuessedWord = guessedWord
+        guessedWord = hangmanLogic(mysteryWord, letterEntered, guessedWord)
+
+        if mysteryWord == guessedWord:
+            win = True
+            break
+
+        if previousGuessedWord == guessedWord:
+            numberOfTries -= 1
+        
+        # niceDisplay(guessedWord, numberOfTries)
 
 def main():
     answer = 'y'
